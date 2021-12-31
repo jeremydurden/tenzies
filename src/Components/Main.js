fbed131315a14887c3d11b65fc0b87cf9e2a797e
Jeremy Durden <jeremydurden@gmail.com>
2021-12-31 12:55:57 -0800

    logic to create an array of 10 randomly generated numbers between 1 and 6 to use for the game dice

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,13 @@
 import Die from "./Die";
 
 function Main() {
+  function allNewDice() {
+    const diceSet = [];
+    while (diceSet.length <= 9) {
+      diceSet.push(Math.floor(Math.random() * (7 - 1) + 1));
+    }
+    return diceSet;
+  }
   return (
     <main className="main__container">
       <div className="main__game-board">
