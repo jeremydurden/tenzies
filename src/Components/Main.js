53638a46e2f8b6e089cd6ab6fb2b0a7c5dc98e21
Jeremy Durden <jeremydurden@gmail.com>
2021-12-31 19:03:21 -0800

    adding a title and instuctions to the game as well as some styling

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,21 +6,28 @@ import Die from "./Die";
 function Main() {
   const [dice, setDice] = useState(allNewDice);
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: uuidv4(),
+    };
+  }
+
   function allNewDice() {
     const diceSet = [];
     while (diceSet.length <= 9) {
-      const randomNumber = Math.floor(Math.random() * (7 - 1) + 1);
-      diceSet.push({
-        value: randomNumber,
-        isHeld: false,
-        id: uuidv4(),
-      });
+      diceSet.push(generateNewDie());
     }
     return diceSet;
   }
 
   function roll() {
-    setDice(allNewDice);
+    setDice((prevState) =>
+      prevState.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   function hold(id) {
@@ -45,6 +52,11 @@ function Main() {
   return (
     <main className="main__container">
       <div className="main__game-board">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="dice">{diceElements}</div>
         <button className="rollButton" onClick={roll}>
           Roll
