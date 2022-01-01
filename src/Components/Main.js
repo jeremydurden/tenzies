ef5627d431e2aa5cdbf6220d9fd3d01724e8fb16
Jeremy Durden <jeremydurden@gmail.com>
2021-12-31 19:08:33 -0800

    new state initiated w/ a useEffect that checks dice state in its dependency array

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import Die from "./Die";
 
 function Main() {
   const [dice, setDice] = useState(allNewDice);
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    console.log("dice state changed");
+  }, [dice]);
 
   function generateNewDie() {
     return {
